c43cf027ebb40c6de747e1cfe8b7d22608c6b4db
Max T <maxtyuterev@gmail.com>
2023-08-21 18:29:56 -0400

    seperate spotify history file utils

--- src/components/spotify-file-provider.tsx
+++ src/components/spotify-file-provider.tsx
@@ -2,27 +2,6 @@
 
 import { Dispatch, SetStateAction, createContext, useMemo, useState } from "react";
 
-type SpotifyHistoryEntry = {
-
-    timestamp: string,
-    username: string,
-    platform: string,
-    ms_played: number,
-    conn_country: string,
-    ip_addr_decrypted: string,
-    user_agent_decrypted: string,
-    master_metadata_track_name?: string,
-    master_metadata_album_artist_name?: string,
-    master_metadata_album_album_name?: string,
-    spotify_track_uri?: string,
-}
-
-export type SpotifyHistoryFile = {
-    path: string,
-    data: SpotifyHistoryEntry[],
-}
-
-
 export type ContextStorage = string | undefined;
 
 type Context = {
