3c3a06cd3f50d4d53ac9bafde4d5d2cee88399a3
Max T <maxtyuterev@gmail.com>
2023-08-26 14:25:48 -0400

    clean up

--- src/app/advanced/page.tsx
+++ src/app/advanced/page.tsx
@@ -2,11 +2,9 @@
 
 import useSpotifyFile from "@/hooks/spotify-file-hook"
 import { SpotifyHistoryEntry, renderTableCell } from "@/utils/spotify-history-file";
-import { Pagination, Spinner, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, getKeyValue } from "@nextui-org/react";
-import { useInfiniteScroll } from "@nextui-org/use-infinite-scroll";
-import { useAsyncList } from "@react-stately/data";
+import { Pagination, Spinner, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
 import { invoke } from "@tauri-apps/api/tauri";
-import { createRef, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Timeline() {
     let spotify = useSpotifyFile();
@@ -17,12 +15,10 @@ export default function Timeline() {
     const [list, setlist] = useState<SpotifyHistoryEntry[]>([]);
 
     useEffect(() => {
-        console.log("running get total pages...")
+
         invoke<number>('get_number_of_spotify_file_pages')
             .then((number_of_pages) => {
-                console.log(`set total pages to ${number_of_pages}`)
                 setTotalPages(number_of_pages);
-                console.log("set page to 1")
                 setPage(1);
             })
             .catch(console.log);
@@ -31,12 +27,9 @@ export default function Timeline() {
 
     useEffect(() => {
         if (page) {
-            console.log(`getting page ${page}...`)
             invoke<SpotifyHistoryEntry[]>('read_spotify_file_page', { page })
                 .then((page_of_entries) => {
-                    console.log("set list")
                     setlist(page_of_entries);
-                    console.log("set loading to false")
                     setIsLoading(false);
                 })
                 .catch(console.log);
