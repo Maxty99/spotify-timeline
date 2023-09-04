c4b20c0dc76a6544c7cbf261765e86c3fb1fc220
Max T <maxtyuterev@gmail.com>
2023-09-04 14:36:03 -0400

    Fix name on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main className="flex flex-1 flex-col items-center justify-center">
 
-      <h1 className="text-5xl font-bold p-5">Welcome to Spotify Timeline</h1>
+      <h1 className="text-5xl font-bold p-5">Welcome to Backtrack</h1>
 
       <p className="max-w-[50%] text-center"> This is a tool I made to better browse the Spotify history files with additional sorting and filtering features. To use it, browse your file system for a JSON file with spotify history data using the folder icon at the top left and use the dropdown to select the file.</p>
     </main>
